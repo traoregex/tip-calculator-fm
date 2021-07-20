eca459848e030c4f8d074916e830c38d3b237ea0
lioneltraore <lioneltraore@bitbucket.org>
2021-07-20 10:46:50 +0000

    add display component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
+import ScreenDisplay from './ScreenDisplay';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex-center">
+      <div className="main-wrapper">
+        <h1>
+          <span className="part-title">spli</span>
+          <span className="part-title">tter</span>
+        </h1>
+        <div className="calculator">
+          <div className="control">
+            left
+          </div>
+          <div className="display">
+            <ScreenDisplay tip={4.27} total={32.79} />
+          </div>
+        </div>
+      </div>
+      <div class="attribution">
+        Challenge by <a rel="noreferrer" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a rel="noreferrer" href="https://github.com/traoregex/tip-calculator-fm">TRAORE Geraud Lionel</a>.
+      </div>
     </div>
   );
 }
---
+++ src/ScreenDisplay.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './ScreenDisplay.scss';
+
+
+function ScreenDisplay(props) {
+    const [tip, setTip] = useState(props.tip ? props.tip : '0.00');
+    const [total, setTotal] = useState(props.total ? props.total : '0.00');
+
+    function reset() {
+        setTip('0.00');
+        setTotal('0.00');
+        console.log('clicked')
+    }
+
+    return (
+        <div className="screen">
+            <div className="top">
+                <div className="output">
+                    <span>
+                        <span className="main-label">Tip Amount</span>
+                        <span className="secondary-label">/ person</span>
+                    </span>
+                    <span className="pricetag">${ tip }</span>
+                </div>
+                <div className="output">
+                    <span>
+                        <span className="main-label">Total</span>
+                        <span className="secondary-label">/ person</span>
+                    </span>
+                    <span className="pricetag">${ total }</span>
+                </div>
+            </div>
+            <div className="bottom">
+                <button onClick={() => reset()}>Reset</button>
+            </div>
+        </div>
+    );
+}
+
+export default ScreenDisplay;
